eb3554771f0e920b274faefe76a23e98519d1698
Albert Clo <cloete.albert@gmail.com>
2024-11-18 02:29:17 +0200

    Add liveness indicator on menu items.

--- view/left.go
+++ view/left.go
@@ -2,6 +2,7 @@ package view
 
 import (
 	"fmt"
+	"github.com/AlbertClo/pylon/types"
 	"strings"
 )
 
@@ -11,12 +12,19 @@ func RenderLeftContent(m Model) string {
 	for i, item := range m.GetMenuItems() {
 		if i == m.GetSelectedItem() {
 			// Selected item
-			menuStr.WriteString(fmt.Sprintf("> %s\n", item.Name))
+			menuStr.WriteString(fmt.Sprintf("> %s %s\n", liveness(item), item.Name))
 		} else {
 			// Unselected item
-			menuStr.WriteString(fmt.Sprintf("  %s\n", item.Name))
+			menuStr.WriteString(fmt.Sprintf("  %s %s\n", liveness(item), item.Name))
 		}
 	}
 
 	return menuStr.String()
+}
+
+func livenessIndicator(item types.MenuItem) string {
+	if item.Running {
+		return "🟢"
+	}
+	return "⚫"
 }
